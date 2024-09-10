a0332650cb4e13c6444a811ce4716a85d97874d2
vandat3012 <luongvandat301220@gmail.com>
2024-09-10 10:22:26 +0700

    #91 api lọc môn học/ giảng viên để lấy lịch free v4

--- src/main/java/com/techzen/techlearn/repository/TechnicalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techzen.techlearn.repository;
-
-import com.techzen.techlearn.entity.TechnicalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechnicalRepository extends JpaRepository<TechnicalEntity,Integer> {
-}
