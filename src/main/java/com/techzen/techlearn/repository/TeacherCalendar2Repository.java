c4d465b278270b0298420d596265eea6b1338951
Acer <nguyenhuynhbaolong8@gmail.com>
2024-09-12 16:55:53 +0700

    fix feature/28 96

--- src/main/java/com/techzen/techlearn/repository/TeacherCalendar2Repository.java
+++ src/main/java/com/techzen/techlearn/repository/TeacherCalendar2Repository.java
@@ -3,6 +3,7 @@ package com.techzen.techlearn.repository;
 import com.techzen.techlearn.entity.Teacher;
 import com.techzen.techlearn.entity.TeacherCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,5 +12,6 @@ import java.util.UUID;
 public interface TeacherCalendar2Repository extends JpaRepository<TeacherCalendar, Integer> {
 
     boolean existsByTeacherAndStartTimeAndEndTime(Teacher teacher, LocalDateTime startTime, LocalDateTime endTime);
-    List<TeacherCalendar> findByTeacher(Teacher teacher);
+    @Query("select tc from TeacherCalendar tc where tc.startTime >= current_time and tc.teacher.id = :id")
+    List<TeacherCalendar> findByTeacherId(UUID id);
 }
