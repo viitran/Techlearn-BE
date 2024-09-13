db1ac1c8e3fe89cd3fe4a85b4b23fb29e35e4fec
lethanhtuan939 <thanhtuanle939#gmail.com>
2024-09-13 15:10:40 +0700

    modify: adjust api student calendar

--- src/main/java/com/techzen/techlearn/service/impl/StudentCalendarServiceImpl.java
+++ src/main/java/com/techzen/techlearn/service/impl/StudentCalendarServiceImpl.java
@@ -1,5 +1,6 @@
 package com.techzen.techlearn.service.impl;
 
+import com.techzen.techlearn.dto.CalendarDTO;
 import com.techzen.techlearn.dto.request.TeacherCalendarRequestDTO2;
 import com.techzen.techlearn.dto.response.TeacherCalendarResponseDTO2;
 import com.techzen.techlearn.entity.StudentCalendar;
@@ -12,12 +13,17 @@ import com.techzen.techlearn.mapper.TeacherCalendarMapper;
 import com.techzen.techlearn.repository.StudentCalendarRepository;
 import com.techzen.techlearn.repository.TeacherCalendarRepository;
 import com.techzen.techlearn.repository.UserRepository;
+import com.techzen.techlearn.service.MailService;
 import com.techzen.techlearn.service.StudentCalendarService;
+import jakarta.mail.MessagingException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -29,9 +35,10 @@ public class StudentCalendarServiceImpl implements StudentCalendarService {
     StudentCalendarRepository studentCalendarRepository;
     TeacherCalendarRepository teacherCalendarRepository;
     TeacherCalendarMapper teacherCalendarMapper;
+    MailService gmailService;
 
     @Override
-    public TeacherCalendarResponseDTO2 addStudentCalendar(TeacherCalendarRequestDTO2 request) {
+    public TeacherCalendarResponseDTO2 addStudentCalendar(TeacherCalendarRequestDTO2 request) throws MessagingException, IOException {
 
         TeacherCalendar calendar = teacherCalendarRepository.findById(Integer.parseInt(request.getId())).orElseThrow(
                 () -> new AppException(ErrorCode.CALENDAR_NOT_EXISTED)
@@ -41,10 +48,23 @@ public class StudentCalendarServiceImpl implements StudentCalendarService {
                 () -> new AppException(ErrorCode.USER_NOT_EXISTED)
         );
 
+        teacherCalendarMapper.updateEntityFromDTO(request, calendar);
+
+        calendar.setId(Integer.parseInt(request.getId()));
         calendar.setStatus(CalendarStatus.BOOKED);
         calendar.setCourseId(Long.parseLong(request.getCourseId()));
         calendar.setChapterId(Long.parseLong(request.getChapterId()));
         calendar.setUser(user);
+
+        CalendarDTO calendarDTO = CalendarDTO.builder()
+                .attendees(List.of("tieuvi200904@gmail.com"))
+                .subject("Test send calendar mail")
+                .description("Hello I'm Tuan dep trai nhat the gioi")
+                .summary("Summary of the meeting")
+                .meetingLink("https://example.com/meeting")
+                .eventDateTime(LocalDateTime.now().plusMinutes(10)) // (10p sau) // LocalDateTime.of(2024, 09, 05, 14, 30)) Ngày cụ thể: 05/09/2024 lúc 14:30
+                .build();
+        gmailService.sendScheduleSuccessEmail(calendarDTO);
 
         return teacherCalendarMapper.toDTO(teacherCalendarRepository.save(calendar));
     }
